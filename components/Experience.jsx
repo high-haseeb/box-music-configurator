39a52f33c805fe087f46dd877f1a8acfb00c6122
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-02-02 02:40:59 +0500

    added loaing screen

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,17 +1,25 @@
-"use client"
+"use client";
 import { Stereo } from "@/public/Steroe";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { Suspense } from "react";
 
 const Experience = () => {
   return (
     <div className="w-screen h-screen">
-      <Canvas className="w-full h-full">
-        <Environment preset="city" />
-        <Stereo />
-        <OrbitControls />
-      </Canvas>
+      <Suspense
+        fallback={
+          <div className="w-screen h-screen bg-black flex items-center justify-center text-white text-9xl">
+            Loading...
+          </div>
+        }
+      >
+        <Canvas className="w-full h-full">
+          <Environment preset="city" />
+          <Stereo />
+          <OrbitControls />
+        </Canvas>
+      </Suspense>
     </div>
   );
 };
